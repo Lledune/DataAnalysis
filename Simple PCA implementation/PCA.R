de60ebc5d816b35c5a1eb23684878c13961f214a
dfghdf89 <krystalwar@hotmail.fr>
2017-11-17 20:20:15 +0100

    PCA implementation ended

--- Simple PCA implementation/PCA.R
+++ Simple PCA implementation/PCA.R
@@ -1,31 +1,22 @@
-#generating sample
-one = rnorm(n = 10, mean = 5, sd = 2)
-two = rnorm(n = 10, mean = 10, sd = 1)
-three = rnorm(n = 10, mean = 1, sd = 2)
-
-Data = cbind(one,two,three)
+library(RCurl)
+library(plotrix)
+#Getting data
+file = getURL("https://raw.githubusercontent.com/dfghdf89/DataAnalysis/master/eurojob.txt")
+Data = read.csv(text = file, sep = "\t")
 
 R = Data
 
-#Mean vector 
-mv = matrix(ncol = 3, nrow = 1)
-for(i in 1:3){
-  mv[i] = mean(R[,i])
-}
-mv = matrix(mv, ncol = 3, nrow = 10, byrow = T)
-
-#Sd vector
-sv = matrix(ncol = 3, nrow = 1)
-for(i in 1:3){
-  sv[i] = sd(R[,i])
-}
-sv = matrix(sv, ncol = 3, nrow = 10, byrow = T)
+#Preparing Data
+R = R[2:ncol(R)]
+rownames(R) = Data[,1]
 
 #R centered 
-Rcs = (R-mv)/sv
+Rcs = scale(R)
+Rcs = as.matrix(Rcs)
 
 #X for factorial analysis
-X = Rcs/sqrt(10)
+X = Rcs/sqrt(nrow(R))
+X = as.matrix(X)
 
 #C correlation
 C = t(X) %*% X
@@ -33,29 +24,43 @@ C = t(X) %*% X
 #Eigenvectors + values 
 eig = eigen(C)
 U = eig$vectors 
+U[,2] = U[,2] *-1    #Manual axis correction
 eigenvalues = eig$values
 
 psi = X %*% U
-gamma = Rcs %*% U #On multiplie Rcs par les vecteurs propres ce qui nous donne les coordonnées dans le nouveau plan.
+gamma = Rcs %*% U #On multiplie Rcs par les vecteurs propres ce qui nous donne les coordonn?es dans le nouveau plan.
 
 
-Ul = matrix(ncol = 3,nrow = 3)
+Ul = matrix(ncol = ncol(R),nrow = nrow(R))
 
 
-Ul = t(X) %*% gamma
+Ul = t(X) %*% psi
 
-for(i in 1:3){
+for(i in 1:ncol(R)){
   Ul[,i] = Ul[,i] / sqrt(eigenvalues[i])
 }
 
-
-plot(psi[,1], psi[,2], main = "PCA implementation", xlab = round(eig$values[1]/3,2), ylab = round(eig$values[2]/3,2))
+plot(gamma[,1], gamma[,2], asp=1, xlim = c(-7, 7), main = "PCA implementation", xlab = round(eig$values[1]/ncol(R),2), ylab = round(eig$values[2]/ncol(R),2))
+text(gamma[,1], gamma[,2], labels=rownames(R), cex= 0.7, pos = 4)
 abline(h=0)
 abline(v=0)
 
-plot(Ul[,1], Ul[,2])
+
+plot(Ul[,1], Ul[,2], xlim = c(-1, 1), ylim = c(-1, 1),  asp=1)
+text(Ul[,1], Ul[,2], labels=colnames(R), cex= 0.7, pos = 3)
+
+draw.circle(0,0,1)
+
 abline(h=0)
 abline(v=0)
+
+for(i in 1:ncol(X)){
+  segments(0, 0, Ul[i,1], Ul[i,2])
+}
 
 #To add new individuals you need to get them to the format of X, so R-mean/sqrt(n)*s
 #Then X+ * Ualpha (eigenvector alpha)
+
+
+colSd <- function (x, na.rm=FALSE) apply(X=x, MARGIN=2, FUN=sd, na.rm=na.rm)
+
